fb736190d60c1e80f679e55ceb66450fa863868b
Shivenwattss <shivenwatts@gmail.com>
2026-09-01 08:57:52 +0530

    Create q2exception.java

---
+++ q2exception.java
@@ -0,0 +1,21 @@
+public class NestedTryCatch {
+    public static void main(String[] args) {
+
+        try {
+            System.out.println("Outer try block");
+
+            try {
+                int a = 10 / 0;
+                System.out.println(a);
+            } catch (ArithmeticException e) {
+                System.out.println("Inner catch: " + e);
+            }
+
+            int[] arr = {1, 2, 3};
+            System.out.println(arr[5]);
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Outer catch: " + e);
+        }
+    }
+}
